e854fb669953608d5ede4e1a518d71abf2be6634
stalien <stalien@yandex.ru>
2024-02-28 19:08:49 +0300

    Initial commit

---
+++ src/main/java/com/example/qeperftestspringmock/QePerftestSpringMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.qeperftestspringmock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QePerftestSpringMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QePerftestSpringMockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qeperftestspringmock/QePerftestSpringMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qeperftestspringmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QePerftestSpringMockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
