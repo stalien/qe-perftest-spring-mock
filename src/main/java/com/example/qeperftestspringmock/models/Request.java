e1c6a0150dc0ae00ba1710f97f21d0b66f62c607
stalien <stalien@yandex.ru>
2024-02-29 19:32:48 +0300

    add data models

---
+++ src/main/java/com/example/qeperftestspringmock/models/Request.java
@@ -0,0 +1,14 @@
+package com.example.qeperftestspringmock.models;
+
+import lombok.Data;
+
+@Data
+public class Request {
+
+    private String appId;
+    private String name;
+    private String inn;
+    private String ogrn;
+    private String kpp;
+
+}
---
+++ src/main/java/com/example/qeperftestspringmock/models/StatusValues.java
@@ -0,0 +1,5 @@
+package com.example.qeperftestspringmock.models;
+
+public enum StatusValues {
+    IN_PROGRESS, REJECTED, DONE, REVOKED, UNDEFINED
+}
