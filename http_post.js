ee127a3ee99c192d019d802d435e615cbb1f8803
stalien <stalien@yandex.ru>
2024-03-01 22:32:24 +0300

    add simple k6 script to load endpoint

---
+++ http_post.js
@@ -0,0 +1,20 @@
+import http from 'k6/http';
+
+export default function () {
+  const url = 'http://192.168.1.69:8080/mock';
+  const payload = JSON.stringify({
+    name: 'ОАО Ромашка',
+    inn: '7707083894',
+    ogrn: '1027700132195',
+    kpp: '973643001',
+    appId: '4097643769930881329',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  http.post(url, payload, params);
+}
