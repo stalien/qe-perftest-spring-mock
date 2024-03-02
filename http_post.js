2af0c0c3b6fc10cc7f9af2f3c33b70b2d8e67b48
stalien <stalien@yandex.ru>
2024-03-02 15:19:42 +0300

    change tested url to localhost

--- http_post.js
+++ http_post.js
@@ -1,7 +1,7 @@
 import http from 'k6/http';
 
 export default function () {
-  const url = 'http://192.168.1.69:8080/mock';
+  const url = 'http://localhost:8080/mock';
   const payload = JSON.stringify({
     name: 'ОАО Ромашка',
     inn: '7707083894',
